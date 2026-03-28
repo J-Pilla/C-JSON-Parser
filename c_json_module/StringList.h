c537ef58fafc2519428c03f89b9c48c72887f5b2
J-Pilla <pilla.jason@gmail.com>
2026-03-28 19:04:00 -0400

    added code for dll exporting/ importing

--- c_json_module/StringList.h
+++ c_json_module/StringList.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "header.h"
+
 typedef struct StringList
 {
 	struct SLNode* first;
@@ -9,9 +11,9 @@ typedef struct StringList
 
 #define EMPTY_STRING_LIST (StringList){ (struct SLNode *)0, 0 }
 
-inline StringList SLNewList() { return EMPTY_STRING_LIST; }
-int SLFreeList(StringList* this);
-const char* SLGetString(StringList* this, int index);
-int SLSetString(StringList* this, const char* value, int index);
-int SLPush(StringList* this, const char* value);
-int SLPop(StringList* this);
+JSON_API inline StringList SLNewList() { return EMPTY_STRING_LIST; }
+JSON_API int SLFreeList(StringList* this);
+JSON_API const char* SLGetString(StringList* this, int index);
+JSON_API int SLSetString(StringList* this, const char* value, int index);
+JSON_API int SLPush(StringList* this, const char* value);
+JSON_API int SLPop(StringList* this);
--- c_json_module/StringMap.h
+++ c_json_module/StringMap.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "header.h"
+
 typedef struct StringMap
 {
 	struct SMNode* first;
@@ -8,9 +10,9 @@ typedef struct StringMap
 
 #define EMPTY_STRING_MAP (StringMap){ (struct SMNode*)0 }
 
-inline StringMap SMNewMap() { return EMPTY_STRING_MAP; }
-int SMFreeMap(StringMap* this);
-const char* SMGetString(StringMap* this, const char* key);
-int SMSetString(StringMap* this, const char* value, const char* key);
-int SMAdd(StringMap* this, const char* value, const char* key);
-int SMRemove(StringMap* this, const char* key);
+JSON_API inline StringMap SMNewMap() { return EMPTY_STRING_MAP; }
+JSON_API int SMFreeMap(StringMap* this);
+JSON_API const char* SMGetString(StringMap* this, const char* key);
+JSON_API int SMSetString(StringMap* this, const char* value, const char* key);
+JSON_API int SMAdd(StringMap* this, const char* value, const char* key);
+JSON_API int SMRemove(StringMap* this, const char* key);
